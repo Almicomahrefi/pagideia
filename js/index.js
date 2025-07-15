ee27a0583f6ff53c7e8af046da4e51c1dc483580
Almicomahrefi <almicomahrefi1927@gmail.com>
2025-07-15 10:34:16 +0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 const title = document.querySelector('.title')
-const text = `Selamat Pagi Dei`.split('')
+const text = `Pagii Deiaa`.split('')
 
 // Create container for better responsive layout
 title.style.display = 'flex'
